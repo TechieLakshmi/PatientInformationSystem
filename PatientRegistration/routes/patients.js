ab924d24d8a2549159bb7f79372c90c3353e4e39
Madhushree123-b <79501735+Madhushree123-b@users.noreply.github.com>
2023-02-27 18:42:27 +0400

    PI-2 User Authentication

--- PatientRegistration/routes/patients.js
+++ PatientRegistration/routes/patients.js
@@ -2,34 +2,52 @@ const express = require("express");
 const router = express.Router();
 const Patient = require("../services/patientService");
 
+
 // post patient information in the system as part of intial patient registration 
 router.post('/register', (req, res)=> {
-    Patient.register(req, (result)=> {
-        res.json(result);
-    })
+    //validate token
+    const userVerified = Patient.validateToken(req, res);
+    if(!userVerified){
+        res.status(401);
+        res.json({'status':'error', 'message':'Invalid Token or user may not have access to this.'});
+    }else{ 
+      Patient.register(req, res);
+    }
 })
-
 //Get patient information using the phone number after intial patient registration
-router.get('/:phoneNo', (req, res)=> {
-    console.log("request reached @get patient info side");
-    Patient.retrieve(req, (result)=> {
-        res.json(result);
-    })
+router.get('/patients/:phoneNo', (req, res)=> {
+    //validate token
+    const userVerified = Patient.validateToken(req, res);
+    if(!userVerified){
+        res.status(401);
+        res.json({'status':'error', 'message':'Invalid Token or user may not have access to this.'});
+    }else{ 
+      Patient.retrieve(req, res);
+    }
 })
 
 //Update patient information using the phone number
-router.put('/', (req, res)=> {
-    Patient.update(req, (result)=> {
-        res.json(result);
-    })
-
+router.put('/patients', (req, res)=> {
+    //validate token
+    const userVerified = Patient.validateToken(req, res);
+    if(!userVerified){
+        res.status(401);
+        res.json({'status':'error', 'message':'Invalid Token or user may not have access to this.'});
+    }else{ 
+      Patient.update(req, res);
+    }
 })
 
 //Delete patient information using the phone number
-router.delete('/', (req, res)=> {
-    Patient.remove(req, (result)=> {
-        res.json(result);
-    })
+router.delete('/patients/:phoneNo', (req, res)=> {
+    //validate token
+    const userVerified = Patient.validateToken(req, res);
+    if(!userVerified){
+        res.status(401);
+        res.json({'status':'error', 'message':'Invalid Token or user may not have access to this.'});
+    }else{ 
+      Patient.remove(req, res);
+    }
 })
 
 module.exports = router;
